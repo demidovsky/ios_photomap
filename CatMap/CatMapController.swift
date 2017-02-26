805f768bfe864c8767aa28bfccc3173b315cc8c7
Dmitry Demidovsky <d.demidovsky@gmail.com>
2017-02-26 12:44:53 +0300

    Базовая карта

---
+++ CatMap/CatMapController.swift
@@ -0,0 +1,36 @@
+//
+//  CatMapController.swift
+//  CatMap
+//
+//  Created by Митрошка on 26.02.17.
+//  Copyright © 2017 dimmy.pro. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class CatMapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
